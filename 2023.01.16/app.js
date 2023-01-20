f4e0e6a73ef95c82b57255692e97a479022bec2b
Kseniia <kvxoxlova2005@mail.ru>
2023-01-20 18:27:37 +0300

    New

---
+++ 2023.01.16/app.js
@@ -0,0 +1,16 @@
+// const http = require("http")
+// http.createServer(function(request,response){
+
+//     response.end("Hello  NodeJS!")
+
+// }).listen(3000, "127.0.0.1",function(){
+//     console.log("Сервер начал прослушивание запросов на порту 3000")
+// })
+
+// const os = require("os")
+
+// let userName= os.userInfo().username
+
+// console.log(userName)
+
+const greeting = require("./greeting")
---
+++ 2023.01.16/greeting.js
@@ -0,0 +1,5 @@
+// let x = 1;
+// let result = x => Math.sin(x);
+// console.log(result(x));
+
+let currentDateTime = new Date();
